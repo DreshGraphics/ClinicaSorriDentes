cdcd5c8ecffe7330e3074f3fffda9ed95ce830fa
Jonathan Costa Matos <37823427+JonathanMatos97@users.noreply.github.com>
2018-05-29 21:41:24 -0300

    Metodo de Salvar do controle de Consulta

--- ClinicaSorriDentesProject/Consulta/salvarControleClinico.php
+++ ClinicaSorriDentesProject/Consulta/salvarControleClinico.php
@@ -5,47 +5,31 @@ require_once '../BancoDeDados/Conexao_Banco_ClinicaSorridentes.php.inc';
 require_once '../util/daoGenerico.php';
 require_once '../Consulta/ProcedimentoDente.php';
 
-$dente = new ProcedimentoDente();
-$dente = new RegistraProcessoDente();
-
 $metodo = $_POST;
 
 if(isset($metodo["procedimento"])){
     $procedimento = $metodo["procedimento"];
-    $dente = $metodo["numDente"];
+    $Numdente = $metodo["numDente"];
     $importancia = $metodo["importancia"];
+    $quantidade = $metodo["quant"];
     $valor = $metodo["valor_unit"];
-    
-    for($i=0; $i<count($metodo['procedimento']); $i++){
-        
-        echo $procedimento[$i].'<br>';
-        echo $dente[$i].'<br>';
-        echo $importancia[$i].'<br>';
-        echo $valor[$i].'<br><br>';
-        
-    }
-    
-    
-    
-    
-    
-  
-    /*
+     
     for( $i=0; $i<count($metodo['procedimento']); $i++ ){
             
-        $resultado = $dente->Salvar($procedimento[$i], $importancia[$i],$valor[$i]);
+        //$resultado = $dente->Salvar($procedimento[$i], $importancia[$i],$valor[$i]);
         
-        
-        $dente->setValor("NUMERO_DENTE", "6");
+        $dente = new ProcedimentoDente();
         $dente->setValor("PROCEDIMENTO", $procedimento[$i]);
+        $dente->setValor("NUMERO_DENTE", $Numdente[$i]);
         $dente->setValor("IMPORTANCIA", $importancia[$i]);
-         $dente->inserir($dente);
-       
+        $dente->setValor("QUANTIDADE", $quantidade[$i]);
+        $dente->setValor("VALOR", $valor[$i]);
+        $resultado = $dente->inserir($dente);       
     }
     
     if($resultado){
         echo "<script>alert('Dados de Processo salvo com Sucesso!');window.location = '../Telas/TelaControleClinico.php';</script>";
     }else{
         echo "<script>alert('Erro ao tentar salvar dados no Banco!');window.location = '../Telas/TelaControleClinico.php';</script>";
-    }*/
+    }
 }
